3f7a4a4e164e022cc8fb4ab56c28b199e75bbe3c
Anton Gramenko <tony1986@yandex.ru>
2016-03-15 10:14:11 +0200

    Added dinamic urls

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -13,9 +13,36 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = patterns ('',
+	#students urls
+	url(r'^$', 'students.views.students_list', name='home'),
+	#students_add
+	url(r'^students/add/$', 'students.views.students_add', name='students_add'),	
+	#students_edit
+	url(r'^students/(?P<sid>\d+)/edit/$',
+		 'students.views.students_edit',
+		 name='students_edit'),
+	#students_delete
+	url(r'^students/(?P<sid>\d+)/delete/$',
+		 'students.views.students_delete',
+		 name='students_delete'),
+
+	#groups urls
+	url(r'^groups/$', 'students.views.groups_list', name='groups'),
+	#groups_add
+	url(r'^groups/add/$', 'students.views.groups_add', name='groups_add'),	
+	#groups_edit
+	url(r'^groups/(?P<sid>\d+)/edit/$',
+		 'students.views.groups_edit',
+		 name='groups_edit'),
+	#groups_delete
+	url(r'^groups/(?P<sid>\d+)/delete/$',
+		 'students.views.groups_delete',
+		 name='groups_delete'),
+
+	#admin page
     url(r'^admin/', admin.site.urls),
-]
+)
